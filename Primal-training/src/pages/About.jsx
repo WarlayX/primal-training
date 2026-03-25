7051b91b101cc36b9b812d64d5aee31b843e9a7f
WarlayX <adewoleodutayo@gmail.com>
2026-03-25 04:13:22 +0100

    about-route-setup

---
+++ Primal-training/src/pages/About.jsx
@@ -0,0 +1,17 @@
+import AboutHero from '../components/about/AboutHero'
+import OurVision from '../components/about/OurVision'
+import DynamicGym from '../components/about/DynamicGym'
+import Reconnect from '../components/about/Reconnect'
+import CTASection from '../components/CTASection'
+
+export default function About() {
+  return (
+    <div>
+      <AboutHero />
+      <OurVision />
+      <DynamicGym />
+      <Reconnect />
+      <CTASection />
+    </div>
+  )
+}
