e3b33a093df01fbb01d0814785ff42ea482f3440
WarlayX <adewoleodutayo@gmail.com>
2026-03-26 01:14:44 +0100

    Reconnect

--- Primal-training/src/components/About/Reconnect.jsx
+++ Primal-training/src/components/About/Reconnect.jsx
@@ -1,6 +1,15 @@
+import ReconnectImage from '../../assets/images/Reconnect.png';
+
 export default function Reconnect() {
   return (
     <section>
+      <div 
+        style={{backgroundImage: `url(${ReconnectImage})`}} 
+        className='h-120 bg-cover bg-center bg-no-repeat flex items-end md:p-5 xl:h-180'>
+          <h2 className='text-white text-left align-bottom font-extrabold tracking-[-0.05em] leading-none py-5 px-5 text-[30px] md:text-[35px] md:w-[67.5%] xl:w-[47.65%] xl:text-[45px]'>
+            WE'VE CREATED A SPACE WHERE YOU CAN RECONNECT WITH YOUR PRIMAL SELF.
+          </h2>
+      </div>
     </section>
   )
 }
