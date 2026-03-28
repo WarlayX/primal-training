05257613e3f8aab6ed03a99e4ad1217c932cac58
WarlayX <adewoleodutayo@gmail.com>
2026-03-28 18:42:48 +0100

    folder-change

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss()
-  ],
-  server: {
-    host: true
-  }
+    tailwindcss(),
+  ], 
 })
